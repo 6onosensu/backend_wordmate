3d8f5a1feeb79ee729b73e74daba1ba761e04b47
darja <darja.portnova@gmail.com>
2025-04-10 13:44:10 +0300

    unit test: for updateduestatus, task: https://trello.com/c/q7gdT8Fd/27-write-tests-for-backend

---
+++ src/modules/user-word/user-word.helpers.spec.ts
@@ -0,0 +1,24 @@
+import { updateDueStatus } from './user-word.helpers';
+import { UserWord } from './entities/user-word.entity';
+import { Repository } from 'typeorm';
+
+describe('updateDueStatus', () => {
+  it('should set due = true if repetitionDate <= now', async () => {
+    const mockUser = {
+      id: 1,
+      repetitionDate: Date.now() - 1000,
+      due: false,
+    };
+
+    const mockRepo = {
+      find: jest.fn().mockResolvedValue([mockUser]),
+      save: jest.fn(),
+    } as unknown as Repository<UserWord>;
+
+    await updateDueStatus(mockRepo);
+
+    expect(mockRepo.find).toHaveBeenCalled();
+    expect(mockUser.due).toBe(true);
+    expect(mockRepo.save).toHaveBeenCalledWith([mockUser]);
+  });
+});
